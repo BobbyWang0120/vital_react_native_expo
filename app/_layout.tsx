da0b11138a670337130f2eb91e8a3b54e5fc55bf
Bobby Wang <yyaustin@bu.edu>
2024-12-06 21:44:13 -0500

    删除index和explore页面

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { Redirect } from 'expo-router';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <Redirect href="/(tabs)/training" />
+    </>
   );
 }
